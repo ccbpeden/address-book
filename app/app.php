97dd65d5b194ef7b0b01053a717389b52de90c9b
Charles Peden <Guest@Epicodus-7D.local>
2017-02-10 15:04:53 -0800

    add functionality to save contact button in contact.html.twig

--- app/app.php
+++ app/app.php
@@ -9,6 +9,11 @@
         $_SESSION['list_of_contacts'] = array();
     }
 
+    if (empty($_SESSION['current_contact'])) {
+        $_SESSION['current_contact'] = array();
+    }
+
+
     $app = new Silex\Application();
     $app->register(new Silex\Provider\TwigServiceProvider(), array('twig.path' => __DIR__.'/../views'));
 
@@ -18,10 +23,16 @@
 
     $app->post("/create_contact", function() use ($app) {
         $new_contact = new Contact($_POST['contact_first_name'], $_POST['contact_last_name'], $_POST['contact_phone'], $_POST['contact_street'], $_POST['contact_city'], $_POST['contact_state'], $_POST['contact_zipcode']);
-        $new_contact->saveContact();
         $_SESSION['current_contact']=array();
         $new_contact->saveCurrentContact();
         return $app['twig']->render('contact.html.twig', array('contacts' => Contact::getCurrentContact()));
+    });
+
+    $app->post("/save_contact", function() use ($app) {
+        $current_contact = Contact::getCurrentContact();
+        $current_contact = $current_contact[0];
+        $current_contact->saveContact();
+        return $app['twig']->render('index.html.twig', array('contacts' => Contact::getAll()));
     });
 
     $app->post("/delete_contacts", function() use ($app) {
